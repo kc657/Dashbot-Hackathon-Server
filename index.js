18f6583c52c03843431bb097e410e78298d4923a
kc657 <kvn.0218@gmail.com>
2017-11-04 20:43:14 -0700

    MS text analysis working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const RtmClient = require('@slack/client').RtmClient
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS
+const cognitiveServices = require('cognitive-services')
 const qs = require('querystring')
 const https = require('https')
 const request = require('request')
@@ -114,70 +115,42 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   if (message.channel === channel) {
     if (message.text !== null) {
       // MICROSOFT TEXT ANALYSIS
-      let sentimentScore
-      let response_handler = function (response) {
-        let body = ''
-        response.on('data', function (d) {
-          body += d
-        })
-        response.on('end', function () {
-          let body_ = JSON.parse(body)
-          let body__ = JSON.stringify(body_, null, '  ')
-        })
-        response.on('error', function (e) {
-          console.log('Error: ' + e.message)
-        })
+      const headers = {
+        'Content-type': 'application/json'
       }
-      let get_sentiments = function (documents) {
-        let body = JSON.stringify(documents)
-        let request_params = {
-          method: 'POST',
-          hostname: uri,
-          path: path,
-          headers: {
-            'Ocp-Apim-Subscription-Key': accessKey
-          }
-        }
-        let req = https.request(request_params, response_handler)
-        req.write(body)
+      const body = {'documents': [{
+            'language': 'en',
+            'id': '1',
+            'text': `${message.text}`
+          }]}
+      const textAnalyticsClient = new cognitiveServices.textAnalytics({
+        apiKey: 'fc7253e3cc8344c6ae12049c0b80773b',
+        endpoint: 'westus.api.cognitive.microsoft.com'
+      })
+      let score
+      textAnalyticsClient.sentiment({
+        headers,
+        body
+      }).then((response) => {
+        console.log(response.documents[0].score)
+        score = response.documents[0].score
+        let req = https.request(config.slackbot_option, function (res) {
+          var chunks = []
+          res.on('data', function (chunk) {
+            chunks.push(chunk)
+          })
+          res.on('end', function () {
+            var body = Buffer.concat(chunks)
+          })
+        })
+        req.write(qs.stringify({ token: config.slackbot_token,
+          channel: 'D7W5R9KJB',
+          text: `You recently said '${message.text}' in the ${channelName} channel. Your message may come off as condescending or rude since it scored a ${score} on our sentiment detection. We strongly advise you to change your message to one of the following suggestions below:` }))
         req.end()
-      }
-      let documents = { 'documents': [
-        { 'id': '1', 'language': 'en', 'text': `${message.text}` }
-      ]}
-      let sentiments = get_sentiments(documents)
-      console.log(sentiments)
-
-      // HISA API DICIONARY
-      var options = { method: 'GET',
-        url: 'http://160.16.100.143:8000/paraphrase/ver1.0/',
-        qs: { q: message.text },
-        headers:
-        { 'postman-token': '34419346-e804-50a0-5c34-b873572d5a28',
-          'cache-control': 'no-cache' }
-      }
-
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error)
-
-        console.log('HISA API   ', body)
+      }).catch((err) => {
+        console.log(err)
       })
 
-      // SLACK PUSH PRIVATE MESSAGE TO USER
-      let req = https.request(config.slackbot_option, function (res) {
-        var chunks = []
-        res.on('data', function (chunk) {
-          chunks.push(chunk)
-        })
-        res.on('end', function () {
-          var body = Buffer.concat(chunks)
-        })
-      })
-      console.log('!!!!!!!!!!!', message);
-      req.write(qs.stringify({ token: config.slackbot_token,
-        channel: 'D7W5R9KJB',
-        text: `You recently said '${message.text}' in the ${channelName} channel. Your message may come off as condescending or rude since it scored a 0.4 on our sentiment detection. We strongly advise you to change your message to one of the following suggestions below:` }))
-      req.end()
     }
   }
 })
